c215b7577eab7ff3b83c7a7466c32eb27acb46df
seojungmin <neraize@gmail.com>
2022-02-15 10:39:56 +0900

    토스트 띄어보기

--- app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.kotlinbasic_20220215
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +17,13 @@ class MainActivity : AppCompatActivity() {
 
 //          로그를 찍어보는 기능 실행 ->btnLog가 클릭될 때 마다 실행
             Log.d("메인화면", "로그 버튼 눌림 ")
+            Log.d("메인화면","추가 로그 찍어보기")
+            Log.e("메인화면","e로 로그찍기")
         }
 
+        btnToast.setOnClickListener {
+            //토스트 버튼이 눌리면, Toast를 만들고 (makeText) 보여주기(show)
+            Toast.makeText(this,"토스트를 띄워봅니다",Toast.LENGTH_SHORT).show()
+        }
     }
 }
